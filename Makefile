19b1238ee437c4ce749a70246308418d7db68aab
Amit <amitbap@gmail.com>
2016-12-03 15:00:39 -0800

    Refactored, added parsing for useful gadgets

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ all: vuln
 
 vuln: vuln.c
 	sudo bash -c "echo 0 > /proc/sys/kernel/randomize_va_space"
-	gcc -g -fno-stack-protector -mpreferred-stack-boundary=2 -o vuln vuln.c
+	gcc -g -fno-stack-protector  -o vuln vuln.c
 
 clean:
 	rm vuln
--- ropscan.py
+++ ropscan.py
@@ -9,18 +9,26 @@ from capstone.x86 import *
 MAX_GADGET_LEN = 8
 MAX_GADGETS = 10000
 MAX_INSTRUCTION_LEN = 11 #32 bytes
-GADGET_TYPES = [X86_INS_RET]#, X86_INS_CALL]
+GADGET_TYPES = [X86_INS_RET, X86_INS_CALL]
 BINARY_RET = "0xc3"
 
 FILTER_INSTR = ["enter", "leave", ".byte"]
+#we need gadgets to set these regs, and one to call syscall
+REQUIRED_GADGETS = ["rax", "rdi", "rsi", "rdx"]
 
 class GadgetList:
-    def __init__(self):
+    def __init__(self, logfile):
         self.set = set()
         self.gadgets = dict()
         self.size = 0
+        self.out = logfile
+        self.useful_gadgets = dict()
         for t in GADGET_TYPES:
             self.gadgets[t] = list()
+
+        for g in REQUIRED_GADGETS:
+            self.useful_gadgets[g] = list()
+        self.useful_gadgets["syscall"] = list()
 
     def serializeInstructions(self, gadget):
         seq = list()
@@ -39,14 +47,60 @@ class GadgetList:
 
         seq = self.serializeInstructions(gadget)
         if seq not in self.set and gadgetID in GADGET_TYPES:
-            logGadget(gadget)
+            while None in gadget: #remove Nones
+                gadget.remove(None)
+
+            self.logGadget(gadget)
             self.gadgets[gadgetID].append(gadget)
             self.set.add(seq)
             self.size += 1
+
+            self.checkGadget(gadget);
             return True
 
         return False
 
+    def checkGadget(self, gadget):
+        for ind in range(0, len(gadget)):
+            i = gadget[ind]
+            g = list(gadget)[ind:] #only need part of this gadget
+            seq = self.serializeInstructions(g)
+            if seq in self.set:
+                continue
+            info = self.getGadgetInfo(g)
+            if i.mnemonic == "pop" and i.op_str.lower() in REQUIRED_GADGETS:
+                self.useful_gadgets[i.op_str.lower()].append(g)
+                self.set.add(seq)
+                print("[*] Found Gadget %s" % info)
+
+            if i.mnemonic == "syscall" and ind+2 == len(gadget): #try to find just syscall ; ret
+                self.useful_gadgets["syscall"].append(g)
+                self.set.add(seq)
+                print("[*] Found Gadget %s" % info)
+                return
+            #TODO add support for mov gadgets
+
+    def logGadget(self, gadget):
+        s = self.getGadgetInfo(gadget)
+        if not s:
+            return
+
+        self.out.write(s)
+        self.out.write("\n")
+
+    def getGadgetInfo(self, gadget):
+        baseAddr = None
+        s = ""
+        for i in gadget:
+            if not baseAddr:
+                baseAddr = i.address
+            s += " %s %s ;" % (i.mnemonic, i.op_str)
+
+        if not baseAddr:
+            return None
+
+        addr = "0x%x:" % baseAddr
+        return addr + s
 
 def printHelp():
     print("Usage: ropscan [binary]")
@@ -94,57 +148,34 @@ def linearScan(roplen, data):
 
     md = Cs(CS_ARCH_X86, arch)
     md.skipdata = True
+    #md.detail = True
     #start = int(0x340)
 
     addr = set()
     for offset in range(0,8): #instructions eventually converge
         print("Scan %d" % offset)
-        instructions = md.disasm(data, 0)
+        instructions = md.disasm(data, offset)
         for i in instructions:
-            if i.mnemonic in FILTER_INSTR:
+            if i.mnemonic in FILTER_INSTR or "j" in i.mnemonic: #filter jumps
                 gadget = collections.deque([None]*roplen, roplen) #reset
                 continue
-
+            '''
             if i.mnemonic == "int" and i.op_str == "0x80" and i.address not in addr:
                 print("\t[*] Found int 0x80: 0x%x" % i.address)
                 addr.add(i.address)
             if (i.mnemonic == "sysenter" or i.mnemonic == "syscall") and i.address not in addr:
                 print("\t[*] Found %s: 0x%x" % (i.mnemonic, i.address))
                 addr.add(i.address)
-
+            '''
             gadget.append(i)
             #print("0x%x:\t%s\t%s [%d]" % (i.address, i.mnemonic, i.op_str, i.id))
-            if i.id in GADGET_TYPES and gadgetList.addGadget(gadget,i.id):
+            if i.id in GADGET_TYPES:
                 #print("\t[*] Found %s" % getGadgetInfo(gadget))
+                if "0x" not in i.op_str and "[" not in i.op_str: #dont want call to have certain args
+                    gadgetList.addGadget(gadget,i.id)
                 gadget = collections.deque([None]*roplen, roplen) #reset
 
-def logGadget(gadget):
-    global out
-
-    s = getGadgetInfo(gadget)
-    if not out or not s:
-        return
-
-    out.write(s)
-    out.write("\n")
-
-def getGadgetInfo(gadget):
-    baseAddr = None
-    s = ""
-    for i in gadget:
-        if i:
-            if not baseAddr:
-                baseAddr = i.address
-            s += " %s %s ;" % (i.mnemonic, i.op_str)
-
-    if not baseAddr:
-        return None
-
-    addr = "0x%x:" % baseAddr
-    return addr + s
-
 def main():
-    global out
     global gadgetList
 
     if len(sys.argv) < 2:
@@ -152,15 +183,16 @@ def main():
         printHelp()
         return 1
 
-    gadgetList = GadgetList()
-    out = open("gadgets.txt", "w")
+    logfile = open("gadgets.txt", "w")
+    gadgetList = GadgetList(logfile)
+
     with open(sys.argv[1], "rb") as f:
         data = f.read()
 
     instructionScan(MAX_GADGETS, data, BINARY_RET)
     linearScan(MAX_GADGET_LEN, data)
 
-    out.close()
+    gadgetList.out.close()
 
     print("Found %d Unique Gadgets" % gadgetList.size)
     return 0
