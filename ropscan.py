adf765e71b037de280b91296bb40ff4365c4cd16
Amit <amitbap@gmail.com>
2016-12-03 18:32:22 -0800

    refactor, fixed instruction scan but takes too long...

--- ropscan.py
+++ ropscan.py
@@ -6,10 +6,11 @@ import platform
 from capstone import *
 from capstone.x86 import *
 
-MAX_GADGET_LEN = 8
+MAX_GADGET_LEN = 6
 MAX_GADGETS = 10000
-MAX_INSTRUCTION_LEN = 11 #32 bytes
-GADGET_TYPES = [X86_INS_RET, X86_INS_CALL]
+MAX_INSTRUCTION_LEN = 10
+MAX_SCANS = 5
+GADGET_TYPES = [X86_INS_RET]# X86_INS_CALL]
 BINARY_RET = "0xc3"
 
 FILTER_INSTR = ["enter", "leave", ".byte"]
@@ -54,7 +55,6 @@ class GadgetList:
             self.gadgets[gadgetID].append(gadget)
             self.set.add(seq)
             self.size += 1
-
             self.checkGadget(gadget);
             return True
 
@@ -92,6 +92,8 @@ class GadgetList:
         baseAddr = None
         s = ""
         for i in gadget:
+            if not i:
+                continue
             if not baseAddr:
                 baseAddr = i.address
             s += " %s %s ;" % (i.mnemonic, i.op_str)
@@ -102,98 +104,98 @@ class GadgetList:
         addr = "0x%x:" % baseAddr
         return addr + s
 
-def printHelp():
-    print("Usage: ropscan [binary]")
+class GadgetScanner:
+    def __init__(self, d, glist):
+        self.data = d
+        self.gadgetList = glist
 
-def instructionScan(roplen, data, instruction): #"0xc3"
-    hex_data =  " ".join(hex(ord(n)) for n in data)
-    #inds = [m.start() for m in re.finditer("0xc3", hex_data)]
+    def instructionScan(self, roplen, data, instruction): #"0xc3"
+        hex_data =  " ".join(hex(ord(n)) for n in data)
+        #inds = [m.start() for m in re.finditer("0xc3", hex_data)]
 
-    inds = []
-    arr = hex_data.split(" ")
-    for i in range(0, len(arr)):
-        if arr[i] == instruction:
-            inds.append(i)
+        inds = []
+        arr = hex_data.split(" ")
+        for i in range(0, len(arr)):
+            if arr[i] == instruction:
+                inds.append(i)
 
-    blob = MAX_INSTRUCTION_LEN * MAX_GADGET_LEN
-    offsets = []
-    for i in inds:
-        offsets.append(i - blob)
+        blob = MAX_INSTRUCTION_LEN * MAX_GADGET_LEN
+        offsets = []
+        for i in inds:
+            start = i - blob
+            offsets.append((start, i+1))
 
-    print("%d ret instructions found" % len(inds))
-    '''
-    prev = offsets[0]
-    if prev < 0:
-        prev = 0
-
-    for i in range(0, len(inds)):
-        if offsets[i] > 0:
-            print("scan on data[%d:%d] (%d bytes)" % (prev, inds[i], len(data[prev:inds[i]+1])))
-            #linearScan(roplen, data[prev:inds[i]+1]) #TODO wrong addresses b/c offsets are wrong...
-            prev = offsets[i]
-    '''
-
-def linearScan(roplen, data):
-    global gadgetList
-    count = 0
-    gadget = collections.deque([None]*roplen, roplen)
-    arch = None
-    if platform.architecture()[0] == "32bit":
-        arch = CS_MODE_32
-    elif platform.architecture()[0] == "64bit":
-        arch = CS_MODE_64
-    else:
-        print("Cannot find platform architecture")
-        sys.exit(1)
-
-    md = Cs(CS_ARCH_X86, arch)
-    md.skipdata = True
-    #md.detail = True
-    #start = int(0x340)
-
-    addr = set()
-    for offset in range(0,8): #instructions eventually converge
-        print("Scan %d" % offset)
-        instructions = md.disasm(data, offset)
-        for i in instructions:
-            if i.mnemonic in FILTER_INSTR or "j" in i.mnemonic: #filter jumps
-                gadget = collections.deque([None]*roplen, roplen) #reset
+        print("%d ret instructions found" % len(inds))
+        for tup in offsets:
+            start = tup[0]
+            end = tup[1]
+            if start < 0:
                 continue
-            '''
-            if i.mnemonic == "int" and i.op_str == "0x80" and i.address not in addr:
-                print("\t[*] Found int 0x80: 0x%x" % i.address)
-                addr.add(i.address)
-            if (i.mnemonic == "sysenter" or i.mnemonic == "syscall") and i.address not in addr:
-                print("\t[*] Found %s: 0x%x" % (i.mnemonic, i.address))
-                addr.add(i.address)
-            '''
-            gadget.append(i)
-            #print("0x%x:\t%s\t%s [%d]" % (i.address, i.mnemonic, i.op_str, i.id))
-            if i.id in GADGET_TYPES:
-                #print("\t[*] Found %s" % getGadgetInfo(gadget))
-                if "0x" not in i.op_str and "[" not in i.op_str: #dont want call to have certain args
-                    gadgetList.addGadget(gadget,i.id)
-                gadget = collections.deque([None]*roplen, roplen) #reset
+            #print("scanning %d->%d" % (start, end))
+            self.linearScan(MAX_GADGET_LEN, data[start:end])
+
+    def linearScan(self, roplen, data):
+        count = 0
+        gadget = collections.deque([None]*roplen, roplen)
+        arch = None
+        if platform.architecture()[0] == "32bit":
+            arch = CS_MODE_32
+        elif platform.architecture()[0] == "64bit":
+            arch = CS_MODE_64
+        else:
+            print("Cannot find platform architecture")
+            sys.exit(1)
+
+        md = Cs(CS_ARCH_X86, arch)
+        md.skipdata = True
+        #md.detail = True
+        #start = int(0x340)
+
+        addr = set()
+        for offset in range(0,MAX_SCANS): #instructions eventually converge
+            #print("Scan %d" % offset)
+            instructions = md.disasm(data, offset)
+            for i in instructions:
+                if i.mnemonic in FILTER_INSTR or "j" in i.mnemonic: #filter jumps
+                    gadget = collections.deque([None]*roplen, roplen) #reset
+                    continue
+
+                gadget.append(i)
+                #print("0x%x:\t%s\t%s [%d]" % (i.address, i.mnemonic, i.op_str, i.id))
+                if i.id in GADGET_TYPES:
+                    #print("\t[*] Found %s" % self.gadgetList.getGadgetInfo(gadget))
+                    if "0x" not in i.op_str and "[" not in i.op_str: #dont want call to have certain args
+                        self.gadgetList.addGadget(gadget,i.id)
+                    gadget = collections.deque([None]*roplen, roplen) #reset
 
 def main():
-    global gadgetList
 
     if len(sys.argv) < 2:
         print("Not enough arguments")
-        printHelp()
+        print("Usage: ropscan [binary]")
         return 1
 
-    logfile = open("gadgets.txt", "w")
-    gadgetList = GadgetList(logfile)
-
+    if (len(sys.argv) > 2):
+        logfile = open(sys.argv[2] + "_gadgets.txt", "w")
+    else:
+        logfile = open("gadgets.txt", "w")
     with open(sys.argv[1], "rb") as f:
         data = f.read()
 
-    instructionScan(MAX_GADGETS, data, BINARY_RET)
-    linearScan(MAX_GADGET_LEN, data)
+    gadgetList = GadgetList(logfile)
+    gadgetScanner = GadgetScanner(data, gadgetList);
+    gadgetScanner.linearScan(MAX_GADGET_LEN, data)
+    '''
+    gl2 = GadgetList(logfile)
+    gs2 = GadgetScanner(data, gl2);
+
+    gadgetScanner.instructionScan(MAX_GADGETS, data, BINARY_RET)
+    gs2.linearScan(MAX_GADGET_LEN, data)
 
     gadgetList.out.close()
 
+    print("Linear Scan: %d unique gadgets\nInstruction Scan: %d unique gadgets" % (gl2.size, gadgetList.size))
+    '''
     print("Found %d Unique Gadgets" % gadgetList.size)
     return 0
 
